ac5de4e0ee4c97ac732bef6084a6fc29078e7259
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2022-02-19 21:36:00 -0800

    added standalone padding top

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,9 +7,9 @@ export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <div className="flex w-full h-screen antialiased divide-x-[1px] divide-zinc-700 bg-zinc-900" style={{WebkitTapHighlightColor: "transparent"}}>
+    <div className="flex w-full h-screen antialiased divide-x-[1px] divide-zinc-700 bg-zinc-900 standalone:pt-[50px]" style={{WebkitTapHighlightColor: "transparent"}}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"  />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"  />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="back-translucent" />
         <link rel="manifest" href="/site.webmanifest" />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        standalone: { raw: "(display-mode: standalone)" },
+      },
+    },
   },
   plugins: [
     plugin(function ({ addVariant, e, postcss }) {
